dfa47842f09391ea004af23bd2adc2a05a9afc79
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-09-26 18:01:10 +0200

    Manual tailwind install and config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { SlCalender } from "react-icons/sl";
-import { MdOutlineEmojiFoodBeverage } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <h1><SlCalender /> Your bookings <MdOutlineEmojiFoodBeverage /></h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><SlCalender className="inline-block text-red-500 align-top" /> Your bookings </h1>
+      
+      <h2> Hello tailwind world! </h2>
     </div>
   );
 }
