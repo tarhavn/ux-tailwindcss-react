c8657b5cd81b65b14688eee1645083568485feb2
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-10-02 19:05:08 +0200

    Imported JSON data as variable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './components/Search';
 import { SlCalender } from "react-icons/sl";
+import appointmentList from './data.json'
 
 function App() {
   return (
     <div className="App container">
       <h1><SlCalender /> Your bookings</h1>
       <Search />
+
+      <ul className="comment">
+        {appointmentList
+          .map(appointment => (
+            <li>
+              <button type="button">Delete</button>
+              <div>
+                <div><b>Pet name: </b>{appointment.petName}</div>
+                <div><b>Appointment date: </b>{appointment.aptDate}</div>
+                <div><b>Owner: </b>{appointment.ownerName}</div>
+                <div><b>Consultation note: </b>{appointment.aptNotes}</div>
+              </div>
+          </li>
+          ))
+        }
+      </ul>
+
     </div>
   );
 }
