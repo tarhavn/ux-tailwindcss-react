2db952477be440e9f1e6b5039242f596473dccd7
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-10-05 13:21:11 +0200

    useCallback and useEffect hooks application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Search from './components/Search';
 import { SlCalender } from "react-icons/sl";
-import appointmentList from './data.json'
 import AppointmentInfo from './components/AppointmentInfo';
+import { useState, useEffect, useCallback } from 'react';
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json)
+      .then(data => {
+        setAppointmentList(data)
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData])
+
   return (
     <div className="App container">
       <h1><SlCalender /> Your bookings</h1>
