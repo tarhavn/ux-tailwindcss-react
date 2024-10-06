a96917a4c07266d3f1ff80047cdd9e0cc97926e3
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2024-10-06 19:03:55 +0200

    Old searchbox with button component removed

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,39 +1,19 @@
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import { useState } from 'react';
-
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
 
 const MyNavbar = ({ query, onQueryChange }) => {
-  const [searchtext, setSearchtext] = useState('');
-
     return (
         <Navbar className="bg-body-tertiary justify-content-between">
           <Container>
             <Row>
               <Col><Button href='/' variant="secondary">Homepage</Button></Col>
-
               <Col><Button variant="success">Appointment</Button></Col>
-
-              <Col>
-                <Form inline>
-                <Form.Control
-                    type="text"
-                    placeholder="keyword"
-                    className=" mr-sm-2"
-                    value={searchtext}
-                    onChange={e => setSearchtext(e.target.value)}
-                  />
-                </Form>
-              </Col>
-
-              <Col><Button type="submit" onClick={() => alert(searchtext)}>Search</Button></Col>
               
               <Col>
               <DropdownButton as={ButtonGroup} title="Sort by" id="bg-nested-dropdown">
@@ -49,7 +29,6 @@ const MyNavbar = ({ query, onQueryChange }) => {
                 onChange={(event) => {onQueryChange(event.target.value)}}
                 placeholder="Search" /></Col>
             </Row>
-
           </Container>
         </Navbar>
     )
